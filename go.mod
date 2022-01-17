e20653a1f598a96f91550598a5ae167885b079fb
Ian Hutchinson <ihutchin@redhat.com>
2022-01-17 21:18:32 +0000

    Setup server folder and initialise logging

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Open-FiSE/go-rest-api
+
+go 1.17
---
+++ server/main.go
@@ -0,0 +1,21 @@
+package main
+
+import {
+
+	"github.com/golang/glog"
+}
+
+func main() {
+
+	// This is needed to make `glog` believe that the flags have already been parsed, otherwise
+	// every log messages is prefixed by an error message stating the the flags haven't been
+	// parsed.
+	_ = flag.CommandLine.Parse([]string{})
+
+	// Always log to stderr by default
+	if err := flag.Set("logtostderr", "true"); err != nil {
+		glog.Infof("Unable to set logtostderr to true")
+	}
+
+	glog.info("Welcome to the beginning of this project")
+}
